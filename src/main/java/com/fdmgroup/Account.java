8b8503b2e9dd6e7ff376b4c8dea50a20de2d0b97
dillon.coleman <coleman399@gmail.com>
2022-04-26 08:50:26 -0700

    added author and account class

---
+++ src/main/java/com/fdmgroup/Account.java
@@ -0,0 +1,37 @@
+package com.fdmgroup;
+
+import java.util.Date;
+
+public class Account {
+    private int accountNumber;
+    private Date openedDate;
+    
+    public Account(int accountNumber, Date openedDate) {
+        this.accountNumber = accountNumber;
+        this.openedDate = openedDate;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Date getOpenedDate() {
+        return openedDate;
+    }
+
+    public void setOpenedDate(Date openedDate) {
+        this.openedDate = openedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Account [accountNumber=" + accountNumber + ", openedDate=" + openedDate + "]";
+    }
+
+    
+    
+}

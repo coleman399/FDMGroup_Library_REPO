31807c674902763d6e04b0bc9634a1fc102ee3d9
Tucker <tuckerhoog@tutanota.com>
2022-04-26 16:47:21 -0400

    connects to DB. adds demo class

--- src/main/java/bookLibrary/Author.java
+++ src/main/java/bookLibrary/Author.java
@@ -1,10 +1,27 @@
 package bookLibrary;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Author")
 public class Author {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="AUTHOR_ID")
+    private int id;
+    @Column(name="AUTHOR_NAME")
     private String authorName;
 
     public Author(final String authorName) {
         this.authorName = authorName;
+    }
+
+    public Author() {
     }
 
     public String getAuthorName() {
---
+++ src/main/java/bookLibrary/Demo.java
@@ -0,0 +1,40 @@
+package bookLibrary;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class Demo {
+
+  static EntityManagerFactory emf;
+  
+  public static List<String> getAuthorName() {
+    EntityManager em = emf.createEntityManager();
+
+    final String jpql = "SELECT a.authorName FROM Author a";
+    final TypedQuery<String> query = em.createQuery(jpql, String.class);
+    final List<String> results = query.getResultList();
+    em.close();
+    return results;
+  }
+
+  public static void main(String[] args) {
+    Author rowling = new Author("J.K. Rowling");
+    Author herbert  = new Author("Frank Herbert");
+    emf = Persistence.createEntityManagerFactory("JPA");
+    EntityManager em = emf.createEntityManager();
+    EntityTransaction t = em.getTransaction();
+    t.begin();
+    rowling = em.merge(rowling);
+    herbert = em.merge(herbert);
+    em.getTransaction().commit();
+    em.close();
+    System.out.println(getAuthorName().toString());
+    emf.close();
+  }
+
+}
